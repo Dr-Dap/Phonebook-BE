55df5bb767fd89a68cc8061ef55d4c042573d7e6
Braden <kmcbhc@gmail.com>
2021-06-29 13:41:10 +1000

    can access all persons, individual persons, delete persona and add via postman

--- index.js
+++ index.js
@@ -1,11 +1,82 @@
 const express = require('express');
+const { response } = require('express');
 const app = express();
 
-app.use(express.json());
+app.use(express.json())
 
-app.get('api/phonebook', (request, response) => {
-    response.json(contacts);
+let persons = [
+    {
+        id: 1,
+        name: "Hester",
+        number: "03131"
+    },
+    {
+        id: 2,
+        name: "Tom",
+        number: "011231"
+    },
+    {
+        id: 3,
+        name: "Wren",
+        number: "03s31"
+    }
+];
+
+app.get('/', (request, response) => {
+    response.send("<h1>Hello There</h1>");
 });
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons);
+});
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id);
+    const person = persons.find(person => person.id === id);
+    
+    if (person) {
+        response.json(person);
+    } else {
+        response.status(404).end();
+    }
+});
+
+app.delete('/api/persons/:id', (req, res) => {
+    const id = Number(req.params.id);
+
+    persons.filter(person => person.id !== id);
+
+    res.status(204).end();
+});
+
+const generateID = () => {
+    return Math.floor(Math.random() * 100000);
+}
+
+app.post('/api/persons', (request, response) => {
+    const newPerson = request.body;
+
+    if (!newPerson) {
+        response.status(404).json({ error: 'content missing'});
+    } else {
+        const person = {
+            name: newPerson.name,
+            number: newPerson.number,
+            id: generateID()
+        }
+
+        persons = persons.concat(person);
+
+        response.json(person);
+    }
+})
+
+app.get('/info', (request, response) => {
+    const personsSum = persons.length;
+    response.send(`<p>Phonebook has ${personsSum} persons</p><p>${Date()}</p>`)
+})
+
+
 
 const PORT = 3001;
 app.listen(PORT, () => {
