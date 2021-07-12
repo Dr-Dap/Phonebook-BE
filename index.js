aee2c3a27aa2f10a32516fcfb910d925733b063e
Braden <kmcbhc@gmail.com>
2021-07-12 14:05:05 +1000

    a

--- index.js
+++ index.js
@@ -95,7 +95,7 @@ app.get('/info', (request, response) => {
     response.send(`<p>Phonebook has ${personsSum} persons</p><p>${Date()}</p>`)
 });
 
-const PORT = 3001;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 });
